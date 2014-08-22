3ae7e36426bf590dd01750faffbc42eb2faa0ba6
hannunehg <georgehannuneh.ps@gmail.com>
2014-08-22 15:50:27 +0300

    adding  looper.sh initial commit from 6th of Aug

---
+++ looper.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# v 0.1
+#
+
+
+constFile=constants
+
+# Read constans
+rodsNum=`grep number_of_rods $constFile | awk '{print $3}'`
+thickness=`grep thickness $constFile | awk '{print $3}'`
+
+orderNum=`grep number_of_pieces_ordered $constFile | awk '{print $3}'`
+completedNum=`grep number_of_pieces_made $constFile | awk '{print $3}'`
+
+# Loop and call PARSER
+for ((i=1; i<=$orderNum-$completedNum; i++))
+do
+	echo "$i"
+	
+	# Call parser
+
+
+	# Check kill signal	
+
+	
+	# Update number of completed pieces
+	#sed -i s/"`grep number_of_pieces_made $constFile`"/"number_of_pieces_made = `expr $completedNum + $i`"/ $constFile
+
+	
+done
+
+
+
